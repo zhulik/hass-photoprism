859c7d2ea5cc8ca557e8813045035dbeea4d1c95
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-03-07 11:41:57 +0100

    Initial implementation, provides count sensors

---
+++ __init__.py
@@ -0,0 +1,57 @@
+"""The Photoprism integration."""
+import asyncio
+import logging
+
+import aiophotoprism
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    CONF_NAME,
+    CONF_USERNAME,
+    CONF_PASSWORD,
+    CONF_URL,
+    CONF_VERIFY_SSL,
+    EVENT_HOMEASSISTANT_STOP,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers.dispatcher import async_dispatcher_send
+
+from .const import (
+    DOMAIN
+)
+
+PLATFORMS = ["sensor"]
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Photoprism component."""
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Photoprism from a config entry."""
+    data = entry.data
+
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
+
+    name = data[CONF_NAME]
+
+    client = aiophotoprism.Photoprism(
+        data[CONF_USERNAME],
+        data[CONF_PASSWORD],
+        url=data[CONF_URL],
+        verify_ssl=data[CONF_VERIFY_SSL],
+    )
+
+    hass.data[DOMAIN][name] = client
+
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
+    
+    return True
